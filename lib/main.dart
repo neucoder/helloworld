623cbcdc34a41c5486e523be7d3475767d6b97ce
ys <neucoder@163.com>
2025-12-03 12:36:05 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter组件初体验',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('头部区域')),
+        body: Container(child: Center(child: Text("这是body区域"))),
+        bottomNavigationBar: Container(
+          color: const Color.fromARGB(255, 245, 247, 160),
+          height: 80,
+          child: Center(child: Text("这是bottomNavigationBar区域")),
+        ),
+      ),
+    ),
+  );
+}
