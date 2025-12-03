4c523c0d6191f2e61904d03e06246a0f301fb6e8
ys <neucoder@163.com>
2025-12-03 14:53:36 +0800

    居中组件

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,21 @@ void main() {
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
-  // 角度转换为弧度
-  double angleToRadians(double angle) {
-    return angle / 180 * 3.14;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('app bar')),
-        body: Container(
-          transform: Matrix4.rotationZ(angleToRadians(5)),
-          alignment: Alignment.center,
-          width: 200,
-          height: 200,
-          margin: EdgeInsets.all(20),
-          decoration: BoxDecoration(
+        appBar: AppBar(title: Center(child: Text('Center示例'))),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
             color: Colors.blue,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.yellowAccent, width: 5),
-          ),
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              color: const Color.fromRGBO(240, 9, 9, 1),
-              fontSize: 20,
+            child: Center(
+              child: Text(
+                '居中内容',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ),
           ),
         ),
