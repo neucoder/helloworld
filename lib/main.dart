4aacc546e31829527fa15767c7c9fa70f04dd63c
ys <neucoder@163.com>
2025-12-03 19:03:22 +0800

    模板

--- lib/main.dart
+++ lib/main.dart
@@ -20,64 +20,7 @@ class _MainPageState extends State<MainPage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Center(child: Text('Text示例'))),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 210, 211, 241),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Text.rich(
-            TextSpan(
-              text: "Hello ",
-              children: [
-                TextSpan(
-                  text: "World",
-                  style: TextStyle(fontSize: 30, color: Colors.red),
-                ),
-                TextSpan(
-                  text: "!",
-                  style: TextStyle(fontSize: 30, color: Colors.green),
-                ),
-              ],
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                decoration: TextDecoration.underline,
-                decorationColor: Colors.red,
-              ),
-            ),
-          ),
-          //
-          // Text(
-          //   '今天天气不错哦，真是一个好日子哈哈哈，窗前明月光，疑是地上霜，举头望明月，低头思故乡。',
-
-          //   maxLines: 2,
-          //   overflow: TextOverflow.ellipsis,
-          //   style: TextStyle(
-          //     fontSize: 30,
-          //     color: Colors.blue,
-          //     fontStyle: FontStyle.italic,
-          //     fontWeight: FontWeight.bold,
-          //     decoration: TextDecoration.underline,
-          //     decorationColor: Colors.red,
-          //   ),
-          // ),
-          // Text(
-          //   'hello world',
-          //   style: TextStyle(
-          //     fontSize: 50,
-          //     color: Colors.blue,
-          //     fontStyle: FontStyle.italic,
-          //     fontWeight: FontWeight.bold,
-          //     decoration: TextDecoration.underline,
-          //     decorationColor: Colors.red,
-          //   ),
-          // ),
-        ),
+        body: Center(child: Text("hello, world!")),
       ),
     );
   }
