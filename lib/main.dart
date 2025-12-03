be5ba2fdb83ead15ce45786b88e3cd13b5bd333e
ys <neucoder@163.com>
2025-12-03 13:18:05 +0800

    有状态组件

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,21 @@ void main() {
 
 // 自定义无状态组件
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter组件初体验',
-      theme: ThemeData(scaffoldBackgroundColor: Colors.purpleAccent),
+      title: 'Flutter组件初体验-有状态组件',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(title: Text('头部区域')),
+        appBar: AppBar(title: Text('头部区域-有状态组件')),
         body: Container(child: Center(child: Text("这是body区域"))),
         bottomNavigationBar: Container(
           color: const Color.fromARGB(255, 245, 247, 160),
