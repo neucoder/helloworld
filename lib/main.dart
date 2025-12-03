70d2ec7c5bc3a571185428d52c62419e80261720
ys <neucoder@163.com>
2025-12-04 00:22:02 +0800

    SingleChildScrollView组件

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,21 @@ class _MainPageState extends State<MainPage> {
 
   late TextEditingController _usernameController;
   late TextEditingController _passwordController;
+  late ScrollController _scrollController;
 
   @override
   void initState() {
     super.initState();
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+    _scrollController = ScrollController();
   }
 
   @override
   void dispose() {
     _usernameController.dispose();
     _passwordController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -34,65 +39,83 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text('登录'))),
-        body: Container(
-          color: const Color.fromARGB(255, 92, 86, 86),
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                onChanged: (value) {
-                  print("用户名：$value");
+        appBar: AppBar(title: Center(child: Text('SingleChildScrollView组件'))),
+        body: Stack(
+          children: [
+            Container(
+              color: const Color.fromARGB(255, 236, 233, 233),
+              padding: EdgeInsets.all(10),
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: Column(
+                  children: List.generate(50, (index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 10),
+                      width: double.infinity,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "我是第${index + 1}个容器",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              child: GestureDetector(
+                onTap: () {
+                  _scrollController.animateTo(
+                    _scrollController.position.maxScrollExtent,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
                 },
-                onSubmitted: (value) {
-                  print("用户名submit：$value");
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  width: 80,
+                  height: 80,
+                  alignment: Alignment.center,
+
+                  child: Text("去底部", style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: GestureDetector(
+                onTap: () {
+                  _scrollController.animateTo(
+                    _scrollController.position.minScrollExtent,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
                 },
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(left: 20),
-                  hintText: '请输入用户名',
-                  fillColor: Colors.red,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(40),
                   ),
+                  width: 80,
+                  height: 80,
+                  alignment: Alignment.center,
+
+                  child: Text("去顶部", style: TextStyle(color: Colors.white)),
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(left: 20),
-                  hintText: '请输入密码',
-                  fillColor: Colors.blue,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 210, 171, 228),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    print("用户名：${_usernameController.text}");
-                    print("密码：${_passwordController.text}");
-                  },
-                  child: Text(
-                    '登录',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
