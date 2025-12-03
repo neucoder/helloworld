f1aaa1b4f6d7201ca086545d932e227fce53b31a
ys <neucoder@163.com>
2025-12-04 00:36:06 +0800

    ListView直接构建

--- lib/main.dart
+++ lib/main.dart
@@ -39,83 +39,23 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text('SingleChildScrollView组件'))),
-        body: Stack(
-          children: [
-            Container(
-              color: const Color.fromARGB(255, 236, 233, 233),
-              padding: EdgeInsets.all(10),
-              child: SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                  children: List.generate(50, (index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: 10),
-                      width: double.infinity,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        "我是第${index + 1}个容器",
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    );
-                  }),
-                ),
+        appBar: AppBar(title: Center(child: Text('ListView组件'))),
+        body: ListView(
+          children: List.generate(100, (index) {
+            return Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 80,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
               ),
-            ),
-            Positioned(
-              top: 10,
-              right: 10,
-              child: GestureDetector(
-                onTap: () {
-                  _scrollController.animateTo(
-                    _scrollController.position.maxScrollExtent,
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.ease,
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  width: 80,
-                  height: 80,
-                  alignment: Alignment.center,
-
-                  child: Text("去底部", style: TextStyle(color: Colors.white)),
-                ),
+              child: Text(
+                "第${index + 1}项",
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
-            ),
-            Positioned(
-              bottom: 10,
-              right: 10,
-              child: GestureDetector(
-                onTap: () {
-                  _scrollController.animateTo(
-                    _scrollController.position.minScrollExtent,
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.ease,
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  width: 80,
-                  height: 80,
-                  alignment: Alignment.center,
-
-                  child: Text("去顶部", style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
-          ],
+            );
+          }),
         ),
       ),
     );
