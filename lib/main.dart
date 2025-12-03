ffdda367c2ce2504557c6e7c7b631c016d2f3be5
ys <neucoder@163.com>
2025-12-03 13:54:21 +0800

    有状态组件的生命周期

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,55 @@ void main() {
   runApp(MainPage());
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
 
-  // 无状态组件只有在创建的时候会调用一次build方法
-  // 参数改变的时候也会调用build方法
+  @override
+  _MainPageState createState() {
+    print("有状态组件createState方法调用");
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    print("有状态组件initState方法调用");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("有状态组件didChangeDependencies方法调用");
+  }
+
+  @override
+  void didUpdateWidget(covariant MainPage oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("有状态组件didUpdateWidget方法调用");
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print("有状态组件deactivate方法调用");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("有状态组件dispose方法调用");
+  }
+
   @override
   Widget build(BuildContext context) {
-    print("无状态组件build方法调用");
-    return MaterialApp(
-      title: 'Flutter组件初体验',
-      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: Text('头部区域')),
-        body: Container(child: Center(child: Text("这是body区域"))),
-        bottomNavigationBar: Container(
-          color: const Color.fromARGB(255, 245, 247, 160),
-          height: 80,
-          child: Center(child: Text("这是bottomNavigationBar区域")),
-        ),
-      ),
-    );
+    print("有状态组件build方法调用");
+    return Container(child: null);
   }
 }
