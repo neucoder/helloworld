dbf645b81e8b75c846178f130bb181302655fc5e
ys <test@test.com>
2025-12-04 19:04:31 +0800

    父子组件通信

--- lib/main.dart
+++ lib/main.dart
@@ -4,196 +4,55 @@ void main() {
   runApp(MainPage());
 }
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  MainAxisAlignment mainAlign = MainAxisAlignment.spaceBetween;
-  CrossAxisAlignment crsAlign = CrossAxisAlignment.center;
-
-  late TextEditingController _usernameController;
-  late TextEditingController _passwordController;
-  late ScrollController _scrollController;
-
-  int _currentIndex = 0;
-  late PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _usernameController = TextEditingController();
-    _passwordController = TextEditingController();
-    _scrollController = ScrollController();
-    _pageController = PageController(initialPage: _currentIndex);
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    _scrollController.dispose();
-    _pageController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text('ListView组件'))),
-        body: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: Container(
-                alignment: Alignment.center,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  // color: Colors.lightBlueAccent,
-                ),
-                child: Stack(
-                  children: [
-                    PageView.builder(
-                      controller: _pageController, // 添加PageController
-                      itemCount: 10,
-                      onPageChanged: (index) {
-                        // 添加页面变化回调
-                        setState(() {
-                          _currentIndex = index;
-                        });
-                      },
-                      itemBuilder: (context, index) {
-                        return Container(
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(
-                            "轮播图${index + 1}",
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        );
-                      },
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          10,
-                          (index) => GestureDetector(
-                            onTap: () {
-                              _currentIndex = index;
-                              _pageController.animateToPage(
-                                index,
-                                duration: Duration(milliseconds: 300),
-                                curve: Curves.ease,
-                              );
-                              setState(() {});
-                            },
-                            child: Container(
-                              width: 10,
-                              height: 10,
-                              margin: EdgeInsets.symmetric(horizontal: 5),
-                              decoration: BoxDecoration(
-                                color: _currentIndex == index
-                                    ? Colors.red
-                                    : Colors.white,
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            SliverToBoxAdapter(child: SizedBox(height: 10)),
-            SliverPersistentHeader(pinned: true, delegate: _stickyCategory()),
-            SliverToBoxAdapter(child: SizedBox(height: 10)),
-            SliverGrid.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              children: List.generate(100, (index) {
-                return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.purpleAccent,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    "商品${index + 1}",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                );
-              }),
-            ),
-          ],
+        appBar: AppBar(title: Text('Hello World')),
+        body: Center(
+          child: Column(
+            children: [
+              Text('父组件', style: TextStyle(fontSize: 24.0, color: Colors.red)),
+              Child(msg: "好言相---劝"),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class _stickyCategory extends SliverPersistentHeaderDelegate {
+class Child extends StatefulWidget {
+  final String msg;
+  const Child({Key? key, required this.msg}) : super(key: key);
+
   @override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
+  _ChildState createState() => _ChildState();
+}
+
+class _ChildState extends State<Child> {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      // 移除固定高度，让内容自适应
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.lightBlueAccent,
-      ),
-      child: Center(
-        child: ListView.builder(
-          itemCount: 30,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Container(
-              width: 100,
-              height: 40, // 设置固定高度
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.orangeAccent,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              child: Text(
-                "分类${index + 1}",
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            );
-          },
-        ),
+      child: Text(
+        "子组件-${widget.msg}",
+        style: TextStyle(fontSize: 16.0, color: Colors.blue),
       ),
     );
   }
-
-  @override
-  double get maxExtent => 80; // 修改为与minExtent相同
-
-  @override
-  double get minExtent => 40;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
-  }
 }
+
+// class Child extends StatelessWidget {
+//   final String? msg;
+//   const Child({Key? key,  this.msg}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Text("子组件-$msg", style: TextStyle(fontSize: 22.0, color: Colors.blue)),
+//     );
+//   }
+// }
