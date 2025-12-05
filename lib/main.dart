cd1e65d2ba50ac0dc5d0f0a19bbaae6cfd3d7694
ys <test@test.com>
2025-12-05 12:53:13 +0800

    基本路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,91 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main(List<String> args) {
   runApp(MainPage());
 }
 
-class MainPage extends StatefulWidget {
-  MainPage({Key? key}) : super(key: key);
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  void initState() {
-    super.initState();
-    _getChannels();
-  }
-
-  List<Map<String, dynamic>> _list = [];
-
-  void _getChannels() async {
-    Response<dynamic> response = await DioUtils().get("/channels");
-    Map<String, dynamic> res = response.data as Map<String, dynamic>;
-    List<dynamic> channels = res["data"]["channels"] as List<dynamic>;
-    _list = channels.cast<Map<String, dynamic>>();
-    print(_list);
-    setState(() {});
-  }
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("频道管理")),
-        body: GridView.extent(
-          padding: EdgeInsets.all(10),
-          maxCrossAxisExtent: 140,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          childAspectRatio: 2,
-          children: _list
-              .map(
-                (e) => Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue[200],
-                    borderRadius: BorderRadius.circular(10),
+    return MaterialApp(home: ListPage());
+  }
+}
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('列表')),
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () {
+                print('点击了列表项$index');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DetailPage();
+                    },
                   ),
-                  child: Text(
-                    e["name"] as String,
-                    style: TextStyle(fontSize: 20, color: Colors.black),
+                );
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '列表项$index',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
-              .toList(),
-        ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-class DioUtils {
-  final Dio _dio = Dio();
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
 
-  DioUtils() {
-    _dio.options.baseUrl = "http://geek.itheima.net/v1_0";
-    _dio.options.connectTimeout = Duration(seconds: 10);
-    _dio.options.receiveTimeout = Duration(seconds: 10);
-    _dio.options.sendTimeout = Duration(seconds: 10);
-    _addInterceptor();
-  }
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
 
-  Future<Response<dynamic>> get(
-    String url, {
-    Map<String, dynamic>? queryParameters,
-  }) {
-    return _dio.get(url, queryParameters: queryParameters);
-  }
-
-  void _addInterceptor() {
-    _dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (context, handler) {
-          // 在发送请求之前做些什么
-          return handler.next(context); // 继续发送请求
-        },
-        onResponse: (context, handler) {
-          // 在收到响应数据之前做些什么
-          if (context.statusCode! >= 200 && context.statusCode! < 300) {
-            handler.next(context);
-            return; // 继续处理响应
-          }
-          handler.reject(
-            DioException(requestOptions: context.requestOptions),
-          ); // 继续处理响应.
-        },
-        onError: (context, handler) {
-          // 当请求失败时做些什么
-          return handler.reject(context); // 继续处理错误
-        },
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('详情')),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('返回列表'),
+        ),
       ),
     );
   }
